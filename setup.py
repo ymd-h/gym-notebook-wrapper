643660322b89bc013e427a6f0d114b9862ecd8da
Yamada <>
2020-06-14 22:41:43 +0900

    Require ipython

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup, find_packages
 
 setup(name="gym-notebook-wrapper",
       version="0.0.1",
-      install_requires=["gym","matplotlib","pyvirtualdisplay"],
+      install_requires=["gym","matplotlib","pyvirtualdisplay","ipython"],
       packages=["gnwapper"])
