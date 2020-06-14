edc7cbddb968290ff9cb38253389a69a02ded169
Yamada <>
2020-06-14 22:39:24 +0900

    Add setup.py for new repo gym-notebook-wrapper

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup, find_packages
+
+setup(name="gym-notebook-wrapper",
+      version="0.0.1",
+      install_requires=["gym","matplotlib","pyvirtualdisplay"],
+      packages=["gnwapper"])
