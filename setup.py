f00ff94fc82445f95f3831529e40c9a8e81857f9
Yamada <>
2020-06-15 20:44:31 +0900

    Add classifiers

--- setup.py
+++ setup.py
@@ -3,4 +3,12 @@ from setuptools import setup, find_packages
 setup(name="gym-notebook-wrapper",
       version="0.0.1",
       install_requires=["gym","matplotlib","pyvirtualdisplay","ipython"],
-      packages=["gnwrapper"])
+      packages=["gnwrapper"],
+      classifiers=["Development Status :: 3 - Alpha",
+                   "Framework :: Jupyter",
+                   "Intended Audience :: Science/Research",
+                   "License :: OSI Approved :: MIT License",
+                   "Operating System :: POSIX :: Linux",
+                   "Programming Language :: Python :: 3 :: Only",
+                   "Topic :: Scientific/Engineering :: Artificial Intelligence",
+                   "Topic :: Software Development :: Libraries"])
