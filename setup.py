14d1b92913f476cb43426fec099849ef6f5c0190
Yamada <>
2020-06-15 19:18:56 +0900

    Fix typo

--- setup.py
+++ setup.py
@@ -3,4 +3,4 @@ from setuptools import setup, find_packages
 setup(name="gym-notebook-wrapper",
       version="0.0.1",
       install_requires=["gym","matplotlib","pyvirtualdisplay","ipython"],
-      packages=["gnwapper"])
+      packages=["gnwrapper"])
