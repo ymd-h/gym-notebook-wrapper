286fcc99d1896a265442f5987e368b38cd5ccaf0
Yamada <>
2020-06-17 08:11:05 +0900

    Version 0.1.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ else:
 
 setup(name="gym-notebook-wrapper",
       author="Yamada Hiroyuki",
-      version="0.0.3",
+      version="0.1.0",
       install_requires=["gym","matplotlib","pyvirtualdisplay","ipython"],
       packages=["gnwrapper"],
       url="https://gitlab.com/ymd_h/gym-notebook-wrapper",
