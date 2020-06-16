607c61e2f58c13223f26a945a558196ac99c02e4
Yamada <>
2020-06-16 19:55:53 +0900

    Use README.md for setup.py description

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
+import os
 from setuptools import setup, find_packages
+
+description = "Wrapper for running/rendering OpenAI Gym on Jupyter Notebook"
+README = os.path.join(os.path.abspath(os.path.dirname(__file__)),'README.md')
+if os.path.exists(README):
+    with open(README,encoding='utf-8') as f:
+        long_description = f.read()
+    long_description_content_type='text/markdown'
+else:
+    warnings.warn("No README.md")
+    long_description =  description
+    long_description_content_type='text/plain'
+
 
 setup(name="gym-notebook-wrapper",
       author="Yamada Hiroyuki",
@@ -13,4 +26,7 @@ setup(name="gym-notebook-wrapper",
                    "Operating System :: POSIX :: Linux",
                    "Programming Language :: Python :: 3 :: Only",
                    "Topic :: Scientific/Engineering :: Artificial Intelligence",
-                   "Topic :: Software Development :: Libraries"])
+                   "Topic :: Software Development :: Libraries"],
+      description=description,
+      long_description=long_description,
+      long_description_content_type=long_description_content_type)
