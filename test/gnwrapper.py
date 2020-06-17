5e63ee002e4597a043b2c998b185efc2f089f55d
Yamada <>
2020-06-17 22:26:18 +0900

    Unit Test for render return

--- test/gnwrapper.py
+++ test/gnwrapper.py
@@ -8,6 +8,10 @@ class VirtualDisplay(self):
         env.gnwrapper.VirtualDisplay(gym.make("CartPole-v1"))
         self.assertNotEqual(os.getenv("DISPLAY"),None)
 
+    def test_render_return(self):
+        env = gnwrapper.VirtualDisplay(gym.make("CartPole-v1"))
+        self.assertNotEqual(env.render(),None)
+
 
 class TestAnimation(unittest.TestCase):
     def test_render(self):
