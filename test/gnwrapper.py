5f559763900ed261b301e6ea6d35e23b86095400
Yamada <>
2020-06-17 22:24:39 +0900

    Unit Test for Virtual Display

--- test/gnwrapper.py
+++ test/gnwrapper.py
@@ -2,6 +2,13 @@ import unittest
 import gnwrapper
 import gym
 
+
+class VirtualDisplay(self):
+    def test_init(self):
+        env.gnwrapper.VirtualDisplay(gym.make("CartPole-v1"))
+        self.assertNotEqual(os.getenv("DISPLAY"),None)
+
+
 class TestAnimation(unittest.TestCase):
     def test_render(self):
         env = gnwrapper.Animation(gym.make("CartPole-v1"))
