a9801bc94219bf5ccc6cc200a0b0b8de08dce868
Yamada <>
2020-06-17 22:18:35 +0900

    UnitTest for LoopAnimation

--- test/gnwrapper.py
+++ test/gnwrapper.py
@@ -15,5 +15,20 @@ class TestAnimation(unittest.TestCase):
             if d:
                 env.reset()
 
+class TestLoopAnimation(unittest.TestCase):
+    def test_render(self):
+        env = gnwrapper.LoopAnimation(gym.make("CartPole-v1"))
+
+        env.reset()
+
+        for _ in range(100):
+            o, r, d, i = env.step(env.action_space.sample())
+            env.render()
+
+            if d:
+                env.reset()
+
+        env.display()
+
 if __name__ == "__main__":
     unittest.main()
